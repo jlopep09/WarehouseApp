1e1152ba68c48342a129706042b1b4b3fd15023b
jlopep09 <jlopep09@estudiantes.unileon.es>
2025-09-17 13:05:14 +0200

    basic post endpints

---
+++ ui/next-app/src/pages/api/product.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+import { pool } from "@/lib/db";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    // getAuth lee la cookie / headers de la request en Pages Router
+    const { userId } = getAuth(req);
+    const name = "product_Name"
+    const description = "product_desc"
+    const img = null;
+    const warehouse_id = 1;
+
+    if (!userId) {
+      return res.status(401).json({ error: "No autenticado" });
+    }
+
+    await pool.query(
+      "INSERT INTO products (name, description, img, warehouse_id) VALUES (?, ?, ?, ?)",
+      [name, description , img, warehouse_id]
+    );
+
+    return res.status(200).json({ success: true, userId });
+  } catch (err) {
+    console.error("Register error:", err);
+    return res.status(500).json({ error: "Error al guardar usuario" });
+  }
+}
---
+++ ui/next-app/src/pages/api/warehouse.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+import { pool } from "@/lib/db";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  try {
+    // getAuth lee la cookie / headers de la request en Pages Router
+    const { userId } = getAuth(req);
+    const name = "product_Name"
+    const description = "product_desc"
+
+    if (!userId) {
+      return res.status(401).json({ error: "No autenticado" });
+    }
+
+    await pool.query(
+      "INSERT INTO warehouses (name, description, owner_id) VALUES (?, ?, ?,)",
+      [name, description , userId]
+    );
+
+    return res.status(200).json({ success: true, userId });
+  } catch (err) {
+    console.error("Register error:", err);
+    return res.status(500).json({ error: "Error al guardar usuario" });
+  }
+}
